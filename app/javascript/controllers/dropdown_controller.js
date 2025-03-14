8cdfd63db36f0596c99069a4e628260cdbaf35db
emilia grama <emiliagrama@gmail.com>
2025-03-14 13:35:24 +0100

    smal deyails

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["menu"];
+
+  connect() {
+    console.log("Dropdown menu connected");
+  }
+
+  toggle() {
+    this.menuTarget.classList.toggle("active");
+  }
+
+  close(event) {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.classList.remove("active");
+    }
+  }
+}
