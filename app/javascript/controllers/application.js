f44fe16bf82e64da38b977974ac52d17e034afa9
emilia grama <emiliagrama@gmail.com>
2025-04-01 11:21:43 +0200

    hero slider working, navbar spaces set, offers paralel, newsletter works, all buttons and text visible on hero slider

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,3 +1,4 @@
 import { Application } from "@hotwired/stimulus"
 const application = Application.start()
+window.Stimulus = application // ✅ expose globally
 export { application }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,7 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
+import CarouselController from "./carousel_controller"
+application.register("carousel", CarouselController)
+
 eagerLoadControllersFrom("controllers", application)
-import NewsletterController from "./newsletter_controller"
-application.register("newsletter", NewsletterController)
