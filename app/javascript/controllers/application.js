55c6e165c2a328365a143d8870e7cae500c32078
emilia grama <emiliagrama@gmail.com>
2025-04-04 21:59:59 +0200

    transparent navbar and some css

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,18 @@
 import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 window.Stimulus = application // ✅ expose globally
+
+// transparent navbar when scroling
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('nav.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+    console.log('Scrolled - class added');
+  } else {
+    navbar.classList.remove('scrolled');
+    console.log('Not Scrolled - class removed');
+  }
+});
+
+
 export { application }
