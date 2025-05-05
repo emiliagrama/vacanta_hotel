00b21b415e544f0b5cf76412b3935e0aeed91f77
emilia grama <emiliagrama@gmail.com>
2025-05-05 22:55:49 +0200

    Redirect all traffic to https://hotelvacanta.ro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  before_action :ensure_canonical_host_and_ssl
+  private
+
+  def ensure_canonical_host_and_ssl
+    canonical = "hotelvacanta.ro"
+    if request.host != canonical || !request.ssl?
+      redirect_to(
+        "https://#{canonical}#{request.fullpath}",
+        status: :moved_permanently,
+        allow_other_host: true    # ← this tells Rails it’s OK
+      )
+    end
+  end
 end
