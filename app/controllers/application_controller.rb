1e61b67594cbad5609955488bac5b6e8173ab30b
emilia grama <emiliagrama@gmail.com>
2025-05-05 23:12:05 +0200

    Revert ApplicationController to default (remove redirect logic)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,4 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  before_action :ensure_canonical_host_and_ssl
-  private
-
-  def ensure_canonical_host_and_ssl
-    canonical = "hotelvacanta.ro"
-    if request.host != canonical || !request.ssl?
-      redirect_to(
-        "https://#{canonical}#{request.fullpath}",
-        status: :moved_permanently,
-        allow_other_host: true    # ← this tells Rails it’s OK
-      )
-    end
-  end
+ 
 end
