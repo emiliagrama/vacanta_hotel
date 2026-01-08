f25dca07a015e1d8042c06c175083f4e5e80a84f
emilia grama <emiliagrama@gmail.com>
2026-01-08 12:40:05 +0100

    Temporarily enable runtime asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.compile = false
+  config.assets.compile = true
+
 
   #config.assets.css_compressor = :sassc
   config.assets.digest = true
