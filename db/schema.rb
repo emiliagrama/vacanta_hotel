4a01af0e00c5410fca51c2b7f4e88152e6cc5cad
emilia grama <emiliagrama@gmail.com>
2026-02-19 18:19:43 +0100

    Update schema after adding intro_text to offer programs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_18_185918) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_19_171131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_18_185918) do
     t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "intro_text"
     t.index ["key"], name: "index_offer_programs_on_key", unique: true
   end
 
